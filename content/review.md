61d7d3f8ab67be29d41cc9643a24f94c5bb0d5e5
lcy-hugepanda <lcy.hugepanda@gmail.com>
2015-10-12 15:57:33 +0800

    sync

--- content/review.md
+++
@@ -1,9 +0,0 @@
-Title: My First Review中文标题
-Date: 2010-12-03 10:20
-Category: Review
-
-Following is a review of my favorite mechanical keyboard.
-测试一下中文的效果
-
-然后测试一下数学公式的效果
-$E = mc^2$
--- pelicanconf.py
+++ pelicanconf.py
@@ -6,73 +6,50 @@ AUTHOR = u'LCY'
 SITENAME = u'LCY Data Science Couple'
 SITETITLE = u'LCY Data Science Couple'
 SITEURL = ''
+DISQUS_URL = "lcy-couple.disqus.com"
+SITESUBTITLE = 'Welcome :-)'
 
 PATH = 'content'
-STATIC_PATHS = ['theme/images', 'images']
+# STATIC_PATHS = ['theme/images', 'images']
 
-TIMEZONE = 'Europe/Paris'
-
-DEFAULT_LANG = u'zh'
+TIMEZONE = 'Asia/Chongqing'
+DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = 20
+AUTHOR_FEED_RSS = None		 
 
 # Theme
 #THEME = "themes/pelican-bootstrap3"
-THEME = "themes/pelican-elegant"
+# THEME = "themes/pelican-elegant"
+THEME = "themes/backdrop"
 
-# About us
-LANDING_PAGE_ABOUT = dict()
-#LANDING_PAGE_ABOUT['title'] = 'Test'
-LANDING_PAGE_ABOUT['details'] = 'Welcome to our tiny site :-) \r\n 我们是'
+# Theme parameters
+# --> color modification in 'backdrop.css':
+#	#71030C --> #005732 (top-bar)
+#	#a21723 --> #007D47 (other)
+# DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'archives', 'search'))
+DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'archives'))
+DEFAULT_PAGINATION = 10
+DEFAULT_ORPHANS = 5
+PAGINATED_DIRECT_TEMPLATES = ('categories', 'archives')
+PROFILE_IMAGE = "images/LCYlogo_Baidu.png"
+BACKDROP_IMAGE = 'images/background.jpg'
+SITE_DESCRIPTION = '我们是LCY Data Science Couple，热爱数据科学和相关技术，欢迎来到我们的小站，有任何问题可以直接联系我们。我们的新浪微博是 @居里猴弟 和 @居里猴姐。'
+SOCIAL=[('Weibo','http://weibo.com/u/1706903234'),
+		('Weibo','http://weibo.com/lcyseso'),
+        ('GitHub','https://github.com/lcy-hugepanda')]
+EMAIL='lcycouple@gmail.com'
 
 # Plugins
 PLUGIN_PATHS = ["plugins"]
-PLUGINS = ["render_math", "representative_image", "ipynb"]
+# PLUGINS = ["render_math", "representative_image", "ipynb"]
+PLUGINS = ["render_math", "representative_image"]
 
-MARKUP = ('md', 'ipynb')
-
-# Pics on the pages
-BANNER = 'images/LCYbanner_1500.png'
-BANNER_ALL_PAGES = True
-SITELOGO = 'images/white.png'
-HIDE_SITENAME = True
-
-# Config of elegant
-MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'headerid', 'toc']
-DIRECT_TEMPLATES = (('index', 'tags', 'categories','archives', 'search', '404'))
-STATIC_PATHS = ['theme/images', 'images']
-TAG_SAVE_AS = ''
-CATEGORY_SAVE_AS = ''
-AUTHOR_SAVE_AS = ''
-
-PROJECTS = [
-	{'name': 'ROC4ML',
-    'url': 'https://github.com/lcy-hugepanda/ROC4ML',
-    'description': 'A simple ROC (Receiver Operating Characteristic) analysis '
-    'and visualization tool for machine learning algorithms.'},
-    {'name': 'MLAlgoTester',
-    'url': 'https://github.com/lcy-hugepanda/MLAlgoTester',
-    'description': 'An automatic evaluating framework for machine learning algorithms'},
-	{'name': 'LCY-ML-Demos',
-    'url': 'https://github.com/lcy-hugepanda/LCY-ML-Demos',
-    'description': 'Some interesting (and useful :) ) demos of machine learning algorithms'}]
+# MARKUP = ('md', 'ipynb')
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
